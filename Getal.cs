977734c345bc00c6a2d2c045d1972c7ddbb40265
hossam01101998 <hossam1998hossam@gmail.com>
2023-10-01 19:08:47 +0200

    Agregar archivos de proyecto.

---
+++ Getal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Eigenschappen
+{
+    internal class Getal
+    {
+
+        public static string Getal1(string input)
+        {
+            string output = "";
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    output += c;
+                }
+            }
+            if (output.Length > 0) { return "Jouw waarden zijn: " + output; }
+            else { return "0 (Er waren geen waarden)"; }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Eigenschappen;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("String naar getal");
+        Console.WriteLine("Schrijf maar iets");
+       
+        string input = Console.ReadLine();
+
+        Console.WriteLine(Getal.Getal1(input));
+
+
+    }
+
+
+}
